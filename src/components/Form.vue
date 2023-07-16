d9f2bdb7a28071d859acb46dc2c5f6806e322cd6
Nassim <you@example.com>
2023-07-16 16:40:19 +0000

    first commit

---
+++ src/components/Form.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+      <h2>Add New Player</h2>
+      <form @submit.prevent="addPlayer" class="player-form">
+        <div class="form-group">
+          <input type="text" placeholder="Player Name" class="form-control" id="playerName" v-model="playerName" required>
+        </div>
+        <div class="form-group">
+          <input type="number" placeholder="Player Number" class="form-control" id="playerNumber" v-model="playerNumber" required>
+        </div>
+        <div class="form-group">
+          <input type="text" placeholder="Player Team" class="form-control" id="playerTeam" v-model="playerTeam" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Add Player</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        playerName: '',
+        playerNumber: '',
+        playerTeam: ''
+      };
+    },
+    methods: {
+      addPlayer() {
+        this.$emit('playerAdded', {
+          name: this.playerName,
+          number: this.playerNumber,
+          team: this.playerTeam
+        });
+        
+        this.playerName = '';
+        this.playerNumber = '';
+        this.playerTeam = '';
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  
+  .player-form {
+    max-width: 500px;
+    padding: 20px;
+    margin: 0 auto;
+    border: 1px solid #ccc;
+    border-radius: 15px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); 
+  }
+  
+  </style>
+  
---
+++ src/components/Navbar.vue
@@ -0,0 +1,78 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Players</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#"
+              >Home <span class="sr-only">(current)</span></a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+          <!-- Dropdown menu -->
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              More
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Option 1</a>
+              <a class="dropdown-item" href="#">Option 2</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Separated link</a>
+            </div>
+          </li>
+        </ul>
+        <ul class="navbar-nav">
+          <!-- Add any additional right-aligned items here -->
+          <li class="nav-item">
+            <a class="nav-link" href="#">Login</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+    /* Custom CSS for sticky header */
+    body {
+      padding-top: 0;
+    }
+    .sticky-top {
+      position: sticky;
+      top: 0;
+      z-index: 1020;
+    }
+  </style>
